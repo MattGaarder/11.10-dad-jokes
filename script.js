3b3824a698ba54508b9739b18760a49596c1e48e
Matteus Gaarder <matteus.gaarder@gmail.com>
2023-08-27 13:13:33 +0100

    refactors code to use async/await rather then .then() syntax

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke();
 
-function generateJoke() {
+async function generateJoke() {
     const config = {
         headers: {
             'Accept': 'application/json'
         }
     }
-    fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then((data) => {
-        jokeEl.innerHTML = data.joke
-    });
-}
+    const res = await fetch('https://icanhazdadjoke.com', config);
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke
+};
+
+// When you use await keyword in a function, make sure to label the function as async
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config).then(res => res.json()).then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     });
+// }
